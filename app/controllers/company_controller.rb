556e5af81455d8064897c5b9ff94475159a6a581
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-06 15:41:00 -0800

    gen company controller

---
+++ app/controllers/company_controller.rb
@@ -0,0 +1,20 @@
+class CompanyController < ApplicationController
+  
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def show
+  end
+end
