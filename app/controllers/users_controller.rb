200a9947fd9084072980f96be7eecadbf9dc380b
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-06 15:39:35 -0800

    generated users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  
+  
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def show
+  end
+end
