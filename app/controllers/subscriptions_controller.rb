b80f150a1a2c9d48f41fcb93cfce653228399596
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-06 19:05:00 -0800

    created subscript form and @variable in contrller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,11 +1,18 @@
 class SubscriptionsController < ApplicationController
+  
+  # show all subscriptions route: '/subscriptions' path: subscriptions_path
   def index
+    @subscriptions = Subscription.all
   end
 
+  # render a new form route: '/subscriptions/new' path: new_subscription_path
   def new
+    @subscription = Subscription.new
   end
 
+  # process submitted new form route: '/subscriptions' POSTS to INDEX route path: subscriptions_path  -- only used on server side
   def create
+    byebug
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create([
-    {username: "ce.sizzle", email: "ce@gmail.com", password: "123"},
-    {username: "mac_daddy", email: "mac@gmail.com", password: "456"},
-    {username: "danimal", email: "dan@gmail.com", password: "789"}
-    ])
+# User.create([
+#     {username: "ce.sizzle", email: "ce@gmail.com", password: "123"},
+#     {username: "mac_daddy", email: "mac@gmail.com", password: "456"},
+#     {username: "danimal", email: "dan@gmail.com", password: "789"}
+#     ])
 
-Company.create([
-    {name: "Netflix"},
-    {name: "MLB"},
-    {name: "Disney"}
-    ])
+# Company.create([
+#     {name: "Netflix"},
+#     {name: "MLB"},
+#     {name: "Disney"}
+#     ])
