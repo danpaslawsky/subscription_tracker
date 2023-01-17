18a3ce85e5d97c3899379c8d4f2e18ffd637b044
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-16 19:05:12 -0800

    moved company attr from user to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :company
+
+    def companies_attributes=(companies_attributes) 
+        companies_attributes.each do |company_attributes|
+          self.companies.build(company_attributes)
+        end
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,5 @@ class User < ApplicationRecord
   has_many :subscriptions
   has_many :companies, through: :subscriptions
 
-  def companies_attributes=(companies_attributes) 
-    companies_attributes.each do |company_attributes|
-      self.companies.build(company_attributes)
-    end
-  end
+  
 end
