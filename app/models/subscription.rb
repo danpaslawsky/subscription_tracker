3ed46c1663efb37dc7fd1bd52c30fad69b7141db
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-18 15:02:47 -0800

    add deletebtn to companies form-refactoring views

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,10 +2,14 @@ class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :company
 
-    def companies_attributes=(companies_attributes) 
-        companies_attributes.each do |company_attributes|
-          self.companies.build(company_attributes)
-        end
-    end
+    accepts_nested_attributes_for :company
     
+    
+    
+    # def companies_attributes=(companies_attributes) 
+    #     companies_attributes.each do |company_attributes|
+    #       self.companies.build(company_attributes)
+    #     end
+    # end
+
 end
